faa40dd8dcec7cff05f586f6e0f6d9b7ee39f935
Laszlo Lazuer <visualjazz@gmail.com>
2019-02-03 18:42:29 -0600

    chore(stub) - ending point for today.

--- src/Components/ClickThrough/index.tsx
+++ src/Components/ClickThrough/index.tsx
@@ -2,15 +2,15 @@ import React, { Component, Fragment } from 'react';
 
 
 interface Props {
-    page?: number
+    page?: Number,
+    words?: any
 };
 
 class ClickThrough extends Component<any,any> {
     constructor(props:Props) {
         super(props);
         this.state = {
-            page: 0
-        }
+            page: 0        }
     }
 
     // Increase page count to simulate move forward
@@ -28,10 +28,22 @@ class ClickThrough extends Component<any,any> {
     }
 
     render() {
+        let words = 'new';
+        switch(this.state.page) {
+            case(0): words= 'first test'
+            break;
+            case(1): words= 'second test'
+            break;
+            case(2): words= 'third test'
+            break;
+            default: console.log('Overflow!')
+            
+        }
         return(
             <Fragment>
                 <h1>Placeholder</h1>
-                <span>Page: {this.state.page}</span>
+                <span>Page: {this.state.page}, case: {words}</span>
+                <br />
                 <button onClick={this.countUp}>+</button>
                 <button onClick={this.countDown}>-</button>
             </Fragment>
